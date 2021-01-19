240e14fc74327abdb5ba79cc79fd123e721a4ad9
xielei <381429578@qq.com>
2021-01-19 20:07:53 +0800

    1

--- src/template/common/sidebar.php
+++
@@ -1,2 +0,0 @@
-{php}echo function_exists('fragment')?fragment('gonggao', '请在后台添加gonggao碎片'):'';{/php}
-{php}echo function_exists('fragment')?fragment('lianxifangshi', '请在后台添加lianxifangshi碎片'):'';{/php}
